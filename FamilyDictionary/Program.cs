0f9e58f4e4afcaf9d3490b7232d43ed4f1c923ec
Andy Million <amillion3@gmail.com>
2018-09-09 07:10:14 -0500

    initial commit

--- FamilyDictionary/Program.cs
+++ FamilyDictionary/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace FamilyDictionary
 {
@@ -6,7 +7,19 @@ namespace FamilyDictionary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            
+        // Define a Dictionary that contains information about several members of your family. Use the following example as a template.
+
+        // Dictionary<string, Dictionary<string, string>> myFamily = new Dictionary<string, Dictionary<string, string>>();
+
+        // myFamily.Add("sister", new Dictionary<string, string>(){
+        //    {"name", "Krista"},
+        //    {"age", "42"}
+        // });
+        //  Next, iterate over each item in myFamily and produce the following output.
+
+        // Krista is my sister and is 42 years old
         }
     }
 }
